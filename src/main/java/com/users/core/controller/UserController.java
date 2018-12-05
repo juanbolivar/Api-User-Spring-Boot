53b5b78e031612be2e8eeadf6e2fc8fe4a60e5de
Juan Bolívar Gázquez <juanboivargazquez@hotmail.com>
2018-12-05 17:00:07 +0100

    Get users

--- src/main/java/com/users/core/controller/UserController.java
+++ src/main/java/com/users/core/controller/UserController.java
@@ -1,12 +1,14 @@
 package com.users.core.controller;
 
 import com.users.core.entity.User;
+import com.users.core.model.MUser;
 import com.users.core.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.List;
 
 @RestController
 @RequestMapping("/v1")
@@ -32,5 +34,10 @@ public class UserController{
     public boolean deleteUser(@PathVariable("id") long id,@PathVariable("name") String name){
 
         return service.delete(id,name);
+    }
+
+    @GetMapping("/users")
+    public List<MUser> obtainUsers(){
+        return service.obtain();
     }
 }
